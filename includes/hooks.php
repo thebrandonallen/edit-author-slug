6a801d3561617ed573c1387688edb2430c850bed
thebrandonallen <cnorris23@gmail.com>
2011-08-17 07:54:57 +0000

    * Drastically improved error handling and feedback for author slug editing * Restore duplicate author slug check as old method could alter the slug without any sort of warning * Further improve the logic for flushing rewrite rules * Introduce ba_eas_can_edit_author_slug() and matching filter to make it even easier to give users the ability to update their own author slug
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/trunk@424711 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/hooks.php
+++ includes/hooks.php
@@ -3,7 +3,7 @@
 /**
  * Edit Author Slug Filters & Actions
  *
- * @package Edit Author Slug
+ * @package Edit_Author_Slug
  * @subpackage Hooks
  *
  * @author Brandon Allen
@@ -12,21 +12,21 @@
 // Admin
 if ( is_admin() ) {
 	// Author base settings
-	add_action( 'admin_init',                 'ba_eas_add_author_base_settings_field'   );
+	add_action( 'admin_init', 'ba_eas_sanitize_author_base'           );
+	add_action( 'admin_init', 'ba_eas_add_author_base_settings_field' );
+
+	// Nicename Actions
+	add_action( 'edit_user_profile',          'ba_eas_show_user_nicename'          );
+	add_action( 'show_user_profile',          'ba_eas_show_user_nicename'          );
+	add_action( 'user_profile_update_errors', 'ba_eas_update_user_nicename', 10, 3 );
 
 	// Nicename column filters
 	add_filter( 'manage_users_columns',       'ba_eas_author_slug_column'               );
 	add_filter( 'manage_users_custom_column', 'ba_eas_author_slug_custom_column', 10, 3 );
 
-	// Nicename Actions
-	add_action( 'edit_user_profile',          'ba_eas_show_user_nicename'               );
-	add_action( 'show_user_profile',          'ba_eas_show_user_nicename'               );
-	add_action( 'edit_user_profile_update',   'ba_eas_update_user_nicename'             );
-	add_action( 'personal_options_update',    'ba_eas_update_user_nicename'             );
 }
 
 // Activation/Deactivation Hooks
-add_action( 'ba_eas_activation',   'ba_eas_cleanup_options' );
-add_action( 'ba_eas_deactivation', 'ba_eas_courtesy_flush'  );
+add_action( 'ba_eas_activation', 'ba_eas_cleanup_options' );
 
 ?>
