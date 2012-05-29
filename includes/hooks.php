6f96b7f8d944f76a608a0447af515eaeccac5f3e
Brandon Allen <cnorris23@gmail.com>
2012-05-29 06:36:36 +0000

    Auto-update code now stable
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/trunk@550129 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/hooks.php
+++ includes/hooks.php
@@ -16,12 +16,12 @@ if ( is_admin() ) {
 	add_action( 'admin_init', 'ba_eas_upgrade', 999 );
 
 	// Nicename Actions
-	add_action( 'edit_user_profile',          'ba_eas_show_user_nicename'          );
-	add_action( 'show_user_profile',          'ba_eas_show_user_nicename'          );
-	add_action( 'user_profile_update_errors', 'ba_eas_update_user_nicename', 10, 3 );
-	add_action( 'admin_head',                 'ba_eas_show_user_nicename_scripts'  );
-	add_action( 'profile_update',              'ba_eas_auto_update_user_nicename'  );
-	add_action( 'user_register',               'ba_eas_auto_update_user_nicename'  );
+	add_action( 'edit_user_profile',          'ba_eas_show_user_nicename'               );
+	add_action( 'show_user_profile',          'ba_eas_show_user_nicename'               );
+	add_action( 'user_profile_update_errors', 'ba_eas_update_user_nicename', 10, 3      );
+	add_action( 'admin_head',                 'ba_eas_show_user_nicename_scripts'       );
+	add_action( 'profile_update',             'ba_eas_auto_update_user_nicename_single' );
+	add_action( 'user_register',              'ba_eas_auto_update_user_nicename_single' );
 
 
 	// Nicename column filters
@@ -29,8 +29,8 @@ if ( is_admin() ) {
 	add_filter( 'manage_users_custom_column', 'ba_eas_author_slug_custom_column', 10, 3 );
 
 	// Settings
-	add_action( 'admin_menu', 'ba_eas_add_settings_menu' );
-	add_action( 'admin_init', 'ba_eas_register_admin_settings'        );
+	add_action( 'admin_menu', 'ba_eas_add_settings_menu'       );
+	add_action( 'admin_init', 'ba_eas_register_admin_settings' );
 
 }
 
