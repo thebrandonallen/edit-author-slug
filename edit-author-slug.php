70b35d231159be80e38407aef4b27007b08a101f
Brandon Allen <cnorris23@gmail.com>
2012-06-11 09:51:28 +0000

    Double check the flaps
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/trunk@556059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edit-author-slug.php
+++ edit-author-slug.php
@@ -15,7 +15,7 @@
  * Plugin Name: Edit Author Slug
  * Plugin URI: http://brandonallen.org/wordpress/plugins/edit-author-slug/
  * Description: Allows an Admin (or capable user) to edit the author slug of a user, and change the Author Base. <em>i.e. - (WordPress default structure) http://example.com/author/username/ (Plugin allows) http://example.com/ninja/master-ninja/</em>
- * Version: 0.9-alpha
+ * Version: 0.9-RC1
  * Tested With: 3.2.1, 3.3.2, 3.4
  * Author: Brandon Allen
  * Author URI: http://brandonallen.org/
@@ -57,14 +57,14 @@ class BA_Edit_Author_Slug {
 	 * @access public
 	 * @var string Edit Author Slug Version
 	 */
-	public $version = '0.9-alpha';
+	public $version = '0.9-RC!';
 
 	/**
 	 * Edit Author Slug Version
 	 * @access public
 	 * @var int Edit Author Slug DB Version
 	 */
-	public $db_version = 130;
+	public $db_version = 132;
 
 	/**
 	 * Edit Author Slug Version
