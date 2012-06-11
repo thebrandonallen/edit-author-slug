99786c0a169c387da392a2ec796dc18efe6557ed
Brandon Allen <cnorris23@gmail.com>
2012-06-11 09:42:38 +0000

    More bumps
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/tags/0.8.1@556055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edit-author-slug.php
+++ edit-author-slug.php
@@ -16,7 +16,7 @@
  * Plugin URI: http://brandonallen.org/wordpress/plugins/edit-author-slug/
  * Description: Allows an Admin (or capable user) to edit the author slug of a user, and change the Author Base. <em>i.e. - (WordPress default structure) http://example.com/author/username/ (Plugin allows) http://example.com/ninja/master-ninja/</em>
  * Version: 0.8.1
- * Tested With: 3.2.1, 3.3.2, 3.4-RC1
+ * Tested With: 3.2.1, 3.3.2, 3.4-RC2
  * Author: Brandon Allen
  * Author URI: http://brandonallen.org/
  * License: GPL2
