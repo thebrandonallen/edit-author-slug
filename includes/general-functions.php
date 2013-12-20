5417aec97950c488b123efbc7e6a3722ff2887f6
Brandon Allen <cnorris23@gmail.com>
2013-12-20 04:04:19 +0000

    Brace yo'self fool, and mind ya own d**n business!
    * Add empty index.php files to prevent snooping for those who haven't turned off indexes
    * Add braces where appropriate to come in line with coding standards. Don't want our pants falling down!
    
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/trunk@825894 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/general-functions.php
+++ includes/general-functions.php
@@ -28,8 +28,9 @@ function ba_eas_do_auto_update() {
 
 	$retval = get_option( '_ba_eas_do_auto_update', '0' );
 
-	if ( !is_numeric( $retval ) || 1 !== (int) $retval )
+	if ( !is_numeric( $retval ) || 1 !== (int) $retval ) {
 		$retval = false;
+	}
 
 	return apply_filters( 'ba_eas_do_auto_update', (bool) $retval );
 }
@@ -55,42 +56,48 @@ function ba_eas_do_auto_update() {
  */
 function ba_eas_auto_update_user_nicename( $user_id, $bulk = false ) {
 	// Bail if there's no id or object
-	if ( empty( $user_id ) )
+	if ( empty( $user_id ) ) {
 		return false;
+	}
 
 	if ( false === $bulk ) {
 		// Should we auto-update
-		if ( !ba_eas_do_auto_update() )
+		if ( !ba_eas_do_auto_update() ) {
 			return false;
+		}
 	}
 
 	// Get WP_User object
 	$user = get_userdata( $user_id );
 
 	// Double check we're still good
-	if ( !is_object( $user ) || empty( $user ) )
+	if ( !is_object( $user ) || empty( $user ) ) {
 		return false;
+	}
 
 	// Setup the user_id
-	if ( !empty( $user->ID ) )
+	if ( !empty( $user->ID ) ) {
 		$user_id  = (int) $user->ID;
 
 	// No user_id so bail
-	else
+	} else {
 		return false;
+	}
 
 	// Get the default nicename structure
 	$structure = apply_filters( 'ba_eas_auto_update_user_nicename_structure', get_option( '_ba_eas_default_user_nicename', 'username' ), $user_id );
 
 	// Make sure we have a structure
-	if ( empty( $structure ) )
+	if ( empty( $structure ) ) {
 		$structure = 'username';
+	}
 
 	// Setup the current nicename
-	if ( empty( $user->user_nicename ) )
+	if ( empty( $user->user_nicename ) ) {
 		$current_nicename = $user->user_nicename;
-	else
+	} else {
 		$current_nicename = $user->user_login;
+	}
 
 	// Setup default nicename
 	$nicename = $current_nicename;
@@ -100,43 +107,49 @@ function ba_eas_auto_update_user_nicename( $user_id, $bulk = false ) {
 
 		case 'username':
 
-			if ( !empty( $user->user_login ) )
+			if ( !empty( $user->user_login ) ) {
 				$nicename = $user->user_login;
+			}
 
 			break;
 
 		case 'nickname':
 
-			if ( !empty( $user->nickname ) )
+			if ( !empty( $user->nickname ) ) {
 				$nicename = $user->nickname;
+			}
 
 			break;
 
 		case 'firstname':
 
-			if ( !empty( $user->first_name ) )
+			if ( !empty( $user->first_name ) ) {
 				$nicename = $user->first_name;
+			}
 
 			break;
 
 		case 'lastname':
 
-			if ( !empty( $user->last_name ) )
+			if ( !empty( $user->last_name ) ) {
 				$nicename = $user->last_name;
+			}
 
 			break;
 
 		case 'firstlast':
 
-			if ( !empty( $user->first_name ) && !empty( $user->last_name ) )
+			if ( !empty( $user->first_name ) && !empty( $user->last_name ) ) {
 				$nicename = $user->first_name . '-' . $user->last_name;
+			}
 
 			break;
 
 		case 'lastfirst':
 
-			if ( !empty( $user->first_name ) && !empty( $user->last_name ) )
+			if ( !empty( $user->first_name ) && !empty( $user->last_name ) ) {
 				$nicename = $user->last_name . '-' . $user->first_name;
+			}
 
 			break;
 	}
@@ -145,8 +158,9 @@ function ba_eas_auto_update_user_nicename( $user_id, $bulk = false ) {
 	$nicename = apply_filters( 'ba_eas_pre_auto_update_user_nicename', sanitize_title( $nicename ), $user_id, $structure );
 
 	// Bail if nothing changed
-	if ( $nicename == $current_nicename )
+	if ( $nicename == $current_nicename ) {
 		return $user_id;
+	}
 
 	// Remove the auto-update actions so we don't find ourselves in a loop
 	remove_action( 'profile_update', 'ba_eas_auto_update_user_nicename_single' );
