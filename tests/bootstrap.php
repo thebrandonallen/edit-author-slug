e48191d12dffc129d8449d1815222c5858757833
Brandon Allen <cnorris23@gmail.com>
2014-02-21 09:11:15 +0000

    First pass at unit tests
    For now, the only test that's run is a test to see if Edit Author Slug is activated. Just wanted to get some groundwork laid out.
    
    git-svn-id: http://plugins.svn.wordpress.org/edit-author-slug/trunk@862209 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Bootstrap the plugin unit testing environment.
+ *
+ * @package WordPress
+ * @subpackage Edit_Author_Slug
+ */
+
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'edit-author-slug/edit-author-slug.php' ),
+);
+
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
+
+if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+} else {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
+}
