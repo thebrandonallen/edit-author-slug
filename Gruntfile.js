b7a128ca80b2e7bffd407614116f54ab68f1f927
Brandon Allen <cnorris23@gmail.com>
2014-08-10 05:03:05 -0500

    Beef up automation/tasks
    * Add Gruntfile.js, package.json, .jshintrc, .editorconfig, and node_modules
    * Rename phpunit.xml to phpunit.xml.dist to play nice with others

---
+++ Gruntfile.js
@@ -0,0 +1,111 @@
+/* jshint node:true */
+module.exports = function(grunt) {
+
+	// Load tasks.
+	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		checktextdomain: {
+			options: {
+				text_domain: 'edit-author-slug',
+				correct_domain: false,
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'_n:1,2,4d',
+					'_ex:1,2c,3d',
+					'_nx:1,2,4c,5d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src: [ '**/*.php', '!node_modules/**/*' ],
+				expand: true
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: ['Gruntfile.js']
+		},
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages',
+					mainFile: 'edit-author-slug.php',
+					potComments: 'Copyright (C) 2014 Brandon Allen\nThis file is distributed under the same license as the Edit Author Slug package.\nSend translations to <wp_plugins [at] brandonallen (dot) org>, 2014.',
+					potFilename: 'edit-author-slug.pot',
+					type: 'wp-plugin'
+				}
+			}
+		},
+		phpunit: {
+			'default': {
+				cmd: 'phpunit',
+				args: ['-c', 'phpunit.xml.dist']
+			},
+			multisite: {
+				cmd: 'phpunit',
+				args: ['-c', 'tests/phpunit/multisite.xml']
+			}
+		},
+		'string-replace': {
+			version: {
+				files: {
+					'edit-author-slug.php': 'edit-author-slug.php',
+					'readme.md': 'readme.md',
+					'readme.txt': 'readme.txt'
+				},
+				options: {
+					replacements: [{
+						pattern: /(\$this->version.*)'(.*)';/gm,
+						replacement: '$1\'<%= pkg.version %>\';'
+					},
+					{
+						pattern: /(\* Version:\s*)(.*)$/gm,
+						replacement: '$1<%= pkg.version %>'
+					},
+					{
+						pattern: /(Stable tag:[\*\ ]*)(.*\S)/gim,
+						replacement: '$1<%= pkg.version %>'
+					}]
+				}
+			}
+		},
+		watch: {
+			js: {
+				files: ['Gruntfile.js'],
+				tasks: ['jshint']
+			}
+		}
+	});
+
+	// Build tasks.
+	grunt.registerTask( 'build', [ 'makepot', 'string-replace:version' ] );
+
+	// PHPUnit test task.
+	grunt.registerMultiTask( 'phpunit', 'Runs PHPUnit tests, including the multisite tests.', function() {
+		grunt.util.spawn( {
+			cmd: this.data.cmd,
+			args: this.data.args,
+			opts: { stdio: 'inherit' }
+		}, this.async() );
+	} );
+
+	// Patch task.
+	grunt.renameTask('patch_wordpress', 'patch');
+
+	// Register the default tasks.
+	grunt.registerTask('default', ['watch']);
+
+};
