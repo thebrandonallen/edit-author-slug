86ba42e09ed6cc0f88c01d0ed9204dc40706a8f9
Brandon Allen <cnorris23@gmail.com>
2015-04-02 14:52:29 -0500

    Add extra directory to check for Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,11 +10,14 @@
 // Support for:
 // 1. `WP_DEVELOP_DIR` environment variable
 // 2. Plugin installed inside of WordPress.org developer checkout
-// 3. Tests checked out to /tmp
+// 3. WordPress.org developer checked out to /tmp
+// 4. Tests checked out to /tmp
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	$_tests_dir = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
 } elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
 	$_tests_dir = '../../../../tests/phpunit';
+} elseif ( file_exists( '/tmp/wordpress/tests/phpunit/includes/bootstrap.php' ) ) {
+	$_tests_dir = '/tmp/wordpress/tests/phpunit';
 } elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
@@ -27,4 +30,3 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-
