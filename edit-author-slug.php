b5369d8a67ecfa3a9f0c4c853816ba54af6a0e08
Brandon Allen <cnorris23@gmail.com>
2015-04-05 18:13:45 -0500

    Reduce license justification

--- edit-author-slug.php
+++ edit-author-slug.php
@@ -25,23 +25,23 @@
  */
 
 /*
-			Copyright 2015  Brandon Allen  (email : wp_plugins ([at]) brandonallen ([dot]) org)
+	Copyright 2015  Brandon Allen  (email : wp_plugins ([at]) brandonallen ([dot]) org)
 
-			This program is free software; you can redistribute it and/or modify
-			it under the terms of the GNU General Public License as published by
-			the Free Software Foundation; either version 2 of the License, or
-			(at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-			This program is distributed in the hope that it will be useful,
-			but WITHOUT ANY WARRANTY; without even the implied warranty of
-			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-			GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-			You should have received a copy of the GNU General Public License
-			along with this program; if not, write to the Free Software
-			Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-			http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+	http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 */
 
 // Exit if accessed directly
@@ -445,3 +445,11 @@ function ba_eas_deactivation() {
 	// Courtesy flush
 	delete_option( 'rewrite_rules' );
 }
+
+function show_user_roles() {
+	echo '<br /><br />';
+	var_dump( absint( '0' ), absint( 'ab4lk56' ), (bool) '0', (bool) '0a' );
+	//var_dump( array_unique( $values ) );
+	echo '<br /><br />';
+}
+add_action( 'admin_footer', 'show_user_roles' );
