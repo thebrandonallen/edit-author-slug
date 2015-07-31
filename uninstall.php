b0396db9465a1c51493a44c2471af09839c32fe2
Brandon Allen <cnorris23@gmail.com>
2015-07-31 02:12:49 -0500

    Clean up uninstall.php
    * Remove extraneous spaces
    * Remove closing tag

--- uninstall.php
+++ uninstall.php
@@ -18,16 +18,14 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 // Delete all the options
-delete_option( '_ba_eas_author_base'           );
-delete_option( '_ba_eas_db_version'            );
-delete_option( '_ba_eas_old_options'           );
-delete_option( '_ba_eas_do_auto_update'        );
+delete_option( '_ba_eas_author_base' );
+delete_option( '_ba_eas_db_version' );
+delete_option( '_ba_eas_old_options' );
+delete_option( '_ba_eas_do_auto_update' );
 delete_option( '_ba_eas_default_user_nicename' );
-delete_option( '_ba_eas_do_role_based'         );
-delete_option( '_ba_eas_role_slugs'            );
-delete_option( 'ba_edit_author_slug'           );
+delete_option( '_ba_eas_do_role_based' );
+delete_option( '_ba_eas_role_slugs' );
+delete_option( 'ba_edit_author_slug' );
 
 // Final flush for good measure
 delete_option( 'rewrite_rules' );
-
-?>
