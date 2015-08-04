d3fca536d63219084753238bdf02d0515c5ee40b
Brandon Allen <cnorris23@gmail.com>
2015-08-04 17:36:16 -0500

    Switch to esc_html for nicename column display

--- includes/admin.php
+++ includes/admin.php
@@ -264,7 +264,7 @@ function ba_eas_author_slug_custom_column( $default, $column_name, $user_id ) {
 		$user = get_userdata( $user_id );
 
 		if ( ! empty( $user->user_nicename ) ) {
-			$default = esc_attr( $user->user_nicename );
+			$default = esc_html( $user->user_nicename );
 		}
 	}
 
