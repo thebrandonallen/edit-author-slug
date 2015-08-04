1e4860c1636aade229e29f1c9f2a52cb2b40c23f
Brandon Allen <cnorris23@gmail.com>
2015-08-04 17:49:50 -0500

    Missed a strict comparison

--- includes/admin.php
+++ includes/admin.php
@@ -149,7 +149,7 @@ function ba_eas_update_user_nicename( $errors, $update, $user ) {
 	$_user = get_userdata( $user->ID );
 
 	// Check for a custom author slug
-	if ( ! empty( $_POST['ba_eas_author_slug'] ) && isset( $_POST['ba_eas_author_slug_custom'] ) && '\c\u\s\t\o\m' == stripslashes( $_POST['ba_eas_author_slug'] ) ) {
+	if ( ! empty( $_POST['ba_eas_author_slug'] ) && isset( $_POST['ba_eas_author_slug_custom'] ) && '\c\u\s\t\o\m' === stripslashes( $_POST['ba_eas_author_slug'] ) ) {
 		$_POST['ba_eas_author_slug'] = $_POST['ba_eas_author_slug_custom'];
 	}
 
