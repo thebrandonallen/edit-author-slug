3b7dc701b10f86db35de2a50b2f5b040d989e03c
Brandon Allen <cnorris23@gmail.com>
2016-02-01 00:36:17 -0600

    Exclude NetBeans project folders in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
 			'!**/bin/**',
 			'!**/build/**',
 			'!**/coverage/**',
+			'!**/nbproject/**',
 			'!**/node_modules/**',
 			'!**/tests/**',
 			'!Gruntfile.js*',
