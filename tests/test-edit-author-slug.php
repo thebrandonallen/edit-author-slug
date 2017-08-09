8424c55e316421bf7321e1ccf8e45c5838771529
Brandon Allen <cnorris23@gmail.com>
2017-08-09 18:58:19 -0500

    Tests: Use `ba_eas()` in test-edit-author-slug.php
    Also remove `EAS_UnitTestCase::setUp()` as it's no longer needed.

--- tests/test-edit-author-slug.php
+++ tests/test-edit-author-slug.php
@@ -12,17 +12,6 @@
 class EAS_UnitTestCase extends WP_UnitTestCase {
 
 	/**
-	 * The `setUp` method.
-	 *
-	 * @since 1.1.0
-	 */
-	public function setUp() {
-		parent::setUp();
-
-		$this->eas = ba_eas();
-	}
-
-	/**
 	 * The admin `tearDown` method.
 	 *
 	 * @since 1.1.0
@@ -31,9 +20,9 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 */
 	public function tearDown() {
 		parent::tearDown();
-		$this->eas->author_base   = 'author';
-		$this->eas->role_slugs    = ba_eas_tests_slugs_default();
-		$this->eas->do_role_based = false;
+		ba_eas()->author_base   = 'author';
+		ba_eas()->role_slugs    = ba_eas_tests_slugs_default();
+		ba_eas()->do_role_based = false;
 	}
 
 	/**
@@ -85,16 +74,16 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 */
 	public function test_setup_globals() {
 		$file = dirname( dirname( __FILE__ ) ) . '/edit-author-slug.php';
-		$this->assertEquals( $file, $this->eas->file );
-		$this->assertEquals( plugin_dir_path( $file ), $this->eas->plugin_dir );
-		$this->assertEquals( plugin_dir_url( $file ), $this->eas->plugin_url );
-		$this->assertEquals( plugin_basename( $file ), $this->eas->plugin_basename );
-		$this->assertEquals( 'author', $this->eas->author_base );
-		$this->assertEquals( 'username', $this->eas->default_user_nicename );
-		$this->assertEquals( false, $this->eas->remove_front );
-		$this->assertEquals( false, $this->eas->do_auto_update );
-		$this->assertEquals( false, $this->eas->do_role_based );
-		// $this->assertEquals( array(), $this->eas->role_slugs );
+		$this->assertEquals( $file, ba_eas()->file );
+		$this->assertEquals( plugin_dir_path( $file ), ba_eas()->plugin_dir );
+		$this->assertEquals( plugin_dir_url( $file ), ba_eas()->plugin_url );
+		$this->assertEquals( plugin_basename( $file ), ba_eas()->plugin_basename );
+		$this->assertEquals( 'author', ba_eas()->author_base );
+		$this->assertEquals( 'username', ba_eas()->default_user_nicename );
+		$this->assertEquals( false, ba_eas()->remove_front );
+		$this->assertEquals( false, ba_eas()->do_auto_update );
+		$this->assertEquals( false, ba_eas()->do_role_based );
+		// $this->assertEquals( array(), ba_eas()->role_slugs );
 	}
 
 	/**
@@ -105,12 +94,12 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 * @covers BA_Edit_Author_Slug::setup_actions
 	 */
 	public function test_setup_actions() {
-		$this->assertEquals( 10, has_action( 'activate_' . $this->eas->plugin_basename, 'ba_eas_activation' ) );
-		$this->assertEquals( 10, has_action( 'deactivate_' . $this->eas->plugin_basename, 'ba_eas_deactivation' ) );
-		$this->assertEquals( 10, has_action( 'after_setup_theme', array( $this->eas, 'set_role_slugs' ) ) );
+		$this->assertEquals( 10, has_action( 'activate_' . ba_eas()->plugin_basename, 'ba_eas_activation' ) );
+		$this->assertEquals( 10, has_action( 'deactivate_' . ba_eas()->plugin_basename, 'ba_eas_deactivation' ) );
+		$this->assertEquals( 10, has_action( 'after_setup_theme', array( ba_eas(), 'set_role_slugs' ) ) );
 		$this->assertEquals( 4,  has_action( 'init', 'ba_eas_wp_rewrite_overrides' ) );
-		$this->assertEquals( 20, has_action( 'init', array( $this->eas, 'add_rewrite_tags' ) ) );
-		$this->assertEquals( 10, has_action( 'plugins_loaded', array( $this->eas, 'load_textdomain' ) ) );
+		$this->assertEquals( 20, has_action( 'init', array( ba_eas(), 'add_rewrite_tags' ) ) );
+		$this->assertEquals( 10, has_action( 'plugins_loaded', array( ba_eas(), 'load_textdomain' ) ) );
 	}
 
 	/**
@@ -124,12 +113,12 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 */
 	public function test__get() {
 
-		$this->assertNull( $this->eas->__get( 'fake_property' ) );
+		$this->assertNull( ba_eas()->__get( 'fake_property' ) );
 
-		$actual = $this->eas->__get( 'version' );
+		$actual = ba_eas()->__get( 'version' );
 		$this->assertSame( BA_Edit_Author_Slug::VERSION, $actual );
 
-		$actual = $this->eas->__get( 'db_version' );
+		$actual = ba_eas()->__get( 'db_version' );
 		$this->assertSame( BA_Edit_Author_Slug::DB_VERSION, $actual );
 	}
 
@@ -147,7 +136,7 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 		$this->assertFalse( is_textdomain_loaded( 'edit-author-slug' ) );
 
 		add_filter( 'override_load_textdomain', array( $this, '_override_load_textdomain_filter' ), 10, 3 );
-		$this->eas->load_textdomain();
+		ba_eas()->load_textdomain();
 		remove_filter( 'override_load_textdomain', array( $this, '_override_load_textdomain_filter' ) );
 
 		$this->assertTrue( is_textdomain_loaded( 'edit-author-slug' ) );
@@ -165,7 +154,7 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 * @expectedDeprecated BA_Edit_Author_Slug::author_base_rewrite
 	 */
 	public function test_author_base_rewrite() {
-		$this->assertNull( $this->eas->author_base_rewrite() );
+		$this->assertNull( ba_eas()->author_base_rewrite() );
 	}
 
 	/**
@@ -176,20 +165,21 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	 * @covers BA_Edit_Author_Slug::set_role_slugs
 	 */
 	public function test_set_role_slugs() {
-		$this->assertEquals( $this->eas->role_slugs, ba_eas_tests_slugs_default() );
+
+		$this->assertEquals( ba_eas()->role_slugs, ba_eas_tests_slugs_default() );
 
 		update_option( '_ba_eas_role_slugs', ba_eas_tests_slugs_custom() );
-		$this->eas->set_role_slugs();
-		$this->assertEquals( $this->eas->role_slugs, ba_eas_tests_slugs_custom() );
+		ba_eas()->set_role_slugs();
+		$this->assertEquals( ba_eas()->role_slugs, ba_eas_tests_slugs_custom() );
 
 		add_role( 'foot-soldier', 'Foot Soldier' );
 		update_option( '_ba_eas_role_slugs', ba_eas_tests_slugs_extra() );
-		$this->eas->set_role_slugs();
-		$this->assertEquals( $this->eas->role_slugs, ba_eas_tests_slugs_extra() );
+		ba_eas()->set_role_slugs();
+		$this->assertEquals( ba_eas()->role_slugs, ba_eas_tests_slugs_extra() );
 		remove_role( 'foot-soldier' );
 
-		$this->eas->set_role_slugs();
-		$this->assertEquals( $this->eas->role_slugs, ba_eas_tests_slugs_default() );
+		ba_eas()->set_role_slugs();
+		$this->assertEquals( ba_eas()->role_slugs, ba_eas_tests_slugs_default() );
 	}
 
 	/**
@@ -202,7 +192,7 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 	public function test_add_rewrite_tags() {
 
 		// Check for return when role-based author base is disabled.
-		$this->assertNull( $this->eas->add_rewrite_tags() );
+		$this->assertNull( ba_eas()->add_rewrite_tags() );
 
 		// Check that rewrite tags have been added when role-based author base is on.
 		$wp_rewrite = $GLOBALS['wp_rewrite'];
@@ -210,28 +200,28 @@ class EAS_UnitTestCase extends WP_UnitTestCase {
 		add_filter( 'ba_eas_do_role_based_author_base', '__return_true' );
 
 		// Test for WP default roles/role slugs.
-		$this->eas->add_rewrite_tags();
+		ba_eas()->add_rewrite_tags();
 		$slugs = '(administrator|editor|author|contributor|subscriber)';
 
 		$this->assertTrue( in_array( '%ba_eas_author_role%', $wp_rewrite->rewritecode, true ) );
 		$this->assertTrue( in_array( $slugs, $wp_rewrite->rewritereplace, true ) );
 
 		$old_author_base = 'test/base';
-		$this->eas->author_base = '%ba_eas_author_role%';
+		ba_eas()->author_base = '%ba_eas_author_role%';
 		$this->assertTrue( in_array( '%ba_eas_author_role%', $wp_rewrite->rewritecode, true ) );
 		$this->assertTrue( in_array( $slugs, $wp_rewrite->rewritereplace, true ) );
-		$this->eas->author_base = $old_author_base;
+		ba_eas()->author_base = $old_author_base;
 
 		// Test for WP custom roles/role slugs.
-		$this->eas->role_slugs = ba_eas_tests_slugs_custom();
-		$this->eas->add_rewrite_tags();
+		ba_eas()->role_slugs = ba_eas_tests_slugs_custom();
+		ba_eas()->add_rewrite_tags();
 		$slugs = '(jonin|chunin|mystic|junior-genin|deshi|author)';
 
 		$this->assertTrue( in_array( $slugs, $wp_rewrite->rewritereplace, true ) );
 
 		// Test for WP custom roles/role slugs.
-		$this->eas->role_slugs = ba_eas_tests_slugs_extra();
-		$this->eas->add_rewrite_tags();
+		ba_eas()->role_slugs = ba_eas_tests_slugs_extra();
+		ba_eas()->add_rewrite_tags();
 		$slugs = '(administrator|editor|author|contributor|subscriber|foot-soldier)';
 
 		$this->assertTrue( in_array( $slugs, $wp_rewrite->rewritereplace, true ) );
