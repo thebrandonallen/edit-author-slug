eb576f7da80c18475ac24ff307fb8f43b8572a0b
Brandon Allen <cnorris23@gmail.com>
2017-11-23 17:32:00 -0500

    Update eslintrc.js
    * Reflect removal of eslint-plugin-wordpress
    * Explicit declaration of Yoda rule is no longer necessary

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
 	root: true,
-	extends: "plugin:wordpress/recommended",
-	plugins: [
-		"wordpress",
-	],
-	rules: {
-		yoda: ["error", "always"]
-	}
+	extends: "wordpress",
 };
