af18afb36d508d2bf46ab083d9550c762616d7b3
Brandon Allen <cnorris23@gmail.com>
2019-02-22 00:38:08 -0600

    Use correct path for PHPUnit 6 compat

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,7 +27,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Compatibility with PHPUnit 6+
  */
 if ( class_exists( 'PHPUnit\Runner\Version' ) ) {
-	require_once dirname( __FILE__ ) . '/phpunit6/compat.php';
+	require_once dirname( __FILE__ ) . '/includes/phpunit6/compat.php';
 }
 
 /**
